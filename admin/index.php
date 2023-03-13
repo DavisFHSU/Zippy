af8313628562fb0b988aac5726ba3a127422c810
DavisFHSU <112444342+DavisFHSU@users.noreply.github.com>
2023-03-13 02:13:48 -0600

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>ZIPPY'S ADMIN</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <!-- <link rel="stylesheet" type="text/css" href="main.css"> -->
+</head>
+<body>
+<div class="container">
+<h2><strong><p class="text-primary">ZIPPY'S USED AUTO</p></strong></h2>
+</body>
+</div>
+</html>
