610d83815fe2c3e103e6da0baa7ccf97b14a30dc
DavisFHSU <112444342+DavisFHSU@users.noreply.github.com>
2023-06-10 21:21:41 -0600

    Update database.php

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@
    // $username = 'inunyh1jgo4tmbbn';
    // $password = 'g6tn59jb73aohzdq';
 
-    $dsn = 'mysql:host=577c9156@us-cdbr-east-06.cleardb.net/heroku_995c6dc43fed76b';
+    $dsn = 'mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_995c6dc43fed76b';
     $username = 'b46689e8632f98';
     $password = '577c9156';
 
