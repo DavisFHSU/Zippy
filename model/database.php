afc7043a510e92e22a85375c2bb12be1830e4be6
DavisFHSU <112444342+DavisFHSU@users.noreply.github.com>
2023-06-10 21:19:16 -0600

    update connection to ClearDB

--- model/database.php
+++ model/database.php
@@ -1,7 +1,11 @@
 <?php
-    $dsn = 'mysql:host=dfkpczjgmpvkugnb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=kbf909vbubz250w8';
-    $username = 'inunyh1jgo4tmbbn';
-    $password = 'g6tn59jb73aohzdq';
+   // $dsn = 'mysql:host=dfkpczjgmpvkugnb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=kbf909vbubz250w8';
+   // $username = 'inunyh1jgo4tmbbn';
+   // $password = 'g6tn59jb73aohzdq';
+
+    $dsn = 'mysql:host=577c9156@us-cdbr-east-06.cleardb.net/heroku_995c6dc43fed76b';
+    $username = 'b46689e8632f98';
+    $password = '577c9156';
 
     try {
         $db = new PDO($dsn, $username, $password);
